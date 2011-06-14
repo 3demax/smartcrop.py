4710081bfd06d757135fceb32f6adbcf98edc034
3demax <3demax@gmail.com>
2011-06-14 21:03:07 +0300

    Add -o option for output file Add check for existense of file with default name

--- compose.py
+++ compose.py
@@ -5,11 +5,11 @@ import Image
 from optparse import OptionParser
 import os
 
-parser = OptionParser(usage="Usage: " + sys.argv[0] + " [FILE ...]")
-#parser.add_option("-d", "--output-directory", dest="directory",
-#				 help="move files to directory after cropping", metavar="DIR")
+parser = OptionParser(usage="Usage: " + sys.argv[0] + " [FILE ...] [-o OUTFILE]")
+parser.add_option("-o", "--output", dest="output",
+				 help="output file name", metavar="OUTFILE")
 (options, args) = parser.parse_args()
-print(options)
+#print(options)
 
 if (len(args) < 1):
 	parser.print_help()
@@ -22,4 +22,15 @@ for ifile in args:
 	tmp.paste(img, (0,0), img)
 
 #tmp.show()
-tmp.save('out.png')
+if (options.output == None):
+	outfile = "out.png"
+	i = 1;
+	while ( os.path.exists(outfile) ):
+		i += 1
+		outfile = "out" + str(i) + ".png"
+	outfile = "out" + str(i) + ".png"
+	print ("writing result to " + outfile)
+else:
+	outfile = options.output
+	
+tmp.save(outfile)
